0c790c45c222b147fb1bd89829f189d7449ea996
maridivi <maridivi89@gmail.com>
2023-01-20 17:57:03 +0000

    Edit font size in paragraph

--- components/UI/Paragraph.js
+++ components/UI/Paragraph.js
@@ -1,6 +1,8 @@
 export default function Paragraph({ children, classname }) {
   return (
-    <p className={`${classname} text-slate-400 text-md lg:text-lg leading-7`}>
+    <p
+      className={`${classname} text-slate-400 text-[16px] lg:text-lg leading-7`}
+    >
       {children}
     </p>
   );
